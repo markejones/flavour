52d36070bc1d3786fa8329302ea66f9348b0e30a
Mark <markjones.js@gmail.com>
2020-03-05 11:19:17 +0000

    Closes #6 (#7)
    * 6: use dotenv-webpack instead of dotenv
    
    * 6: use system vars for ci

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,8 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-const dotenv = require("dotenv");
-const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = () => {
-  const env = dotenv.config().parsed;
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
-
   return {
     module: {
       rules: [
@@ -27,7 +20,9 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "src", "index.html")
       }),
-      new webpack.DefinePlugin(envKeys)
+      new Dotenv({
+        systemvars: true
+      })
     ]
   };
 };
